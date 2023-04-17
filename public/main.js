1907c20c9967390479af0cbdce40ce428f67df22
hiukongDan <ifsoboy9148@hotmail.com>
2023-04-17 16:21:15 +1000

    fixed obstacle collisions

--- public/main.js
+++ public/main.js
@@ -8,14 +8,14 @@ console.log("Hello world");
 /** module facing -y imported from blender */
 const objLoader = new OBJLoader();
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 100 );
 camera.position.copy(new THREE.Vector3(0, 2, 1));
 camera.lookAt(new THREE.Vector3(0,0,0));
 //camera.position.z = 1;
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xcccccc );
-scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+scene.background = new THREE.Color( 0xadadad );
+scene.fog = new THREE.FogExp2( 0xadadad, 0.01 );
 
 const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
@@ -37,6 +37,7 @@ orbitControls.enableDamping = true; // an animation loop is required when either
 orbitControls.dampingFactor = 0.05;
 orbitControls.screenSpacePanning = false;
 orbitControls.maxDistance = 2;
+orbitControls.minDistance = 2;
 orbitControls.maxPolarAngle = Math.PI / 3;
 orbitControls.minPolarAngle = Math.PI/5;
 orbitControls.enableZoom = false;
@@ -67,6 +68,16 @@ grassTexture.wrapS = THREE.RepeatWrapping;
 grassTexture.wrapT = THREE.RepeatWrapping;
 grassTexture.repeat.set(600, 600);
 
+var brickTexture = textureLoader.load("/assets/bricks.png");
+brickTexture.wrapS = THREE.RepeatWrapping;
+brickTexture.wrapT = THREE.RepeatWrapping;
+brickTexture.repeat.set(600, 600);
+
+var groundTexture = textureLoader.load("/assets/ground.png");
+groundTexture.wrapS = THREE.RepeatWrapping;
+groundTexture.wrapT = THREE.RepeatWrapping;
+groundTexture.repeat.set(600, 600);
+
 var raycaster = new THREE.Raycaster();
 
 var commonMaterial = new THREE.MeshToonMaterial({color: 0x8F9779});
@@ -77,7 +88,18 @@ var floorObj = new THREE.Mesh(floorMesh, floorMaterial);
 floorObj.scale.x = 100;
 floorObj.scale.z = 100;
 floorObj.position.y = -0.1;
-scene.add(floorObj);
+//scene.add(floorObj);
+
+var groundMaterial = new THREE.MeshToonMaterial({color: 0xffffff, map: groundTexture});
+var groundObj = new THREE.Mesh(floorMesh, groundMaterial);
+groundObj.scale.x = 100;
+groundObj.scale.z = 100;
+groundObj.position.y = -0.1;
+scene.add(groundObj);
+
+var obstacleGeo = new THREE.BoxGeometry(2,2,2);
+var obstacleMaterial = new THREE.MeshToonMaterial({color: 0xffffff, map:brickTexture});
+
 
 var isTurnLeft = false;
 var isTurnRight = false;
@@ -88,6 +110,25 @@ var isFiring = false;
 var currentSpeed = 0;
 var maxSpeed = 0.01;
 
+var obstacles = new THREE.Group();
+
+
+/** obstacles */
+function initObstacles(){
+   for (var i = -40; i <= 40;){
+      for (var j = -40; j <= 40;){
+         const cube = new THREE.Mesh( obstacleGeo, obstacleMaterial);
+         cube.position.set(i, 1, j);
+         obstacles.add(cube);
+
+         j += Math.random()*10;
+      }
+      i += Math.random()*10;
+   }
+   scene.add(obstacles);
+}
+
+initObstacles();
 
 addEventListener("mousedown", event=>{
    isMouseDown = true;
@@ -154,7 +195,7 @@ pointerLockControl.addEventListener("change", function(event){
 
 addEventListener('keydown', function(event) {
    if (event.code == 'KeyA') {
-     console.log("turn left");
+     //console.log("turn left");
      isTurnLeft = true;
      isTurnRight = false;
    }
@@ -164,7 +205,7 @@ addEventListener('keydown', function(event) {
       isMoveBackward = true;
    }
    else if(event.code == 'KeyD'){
-      console.log("turn right");
+      //console.log("turn right");
       isTurnLeft = false;
       isTurnRight = true;
    }
@@ -174,34 +215,34 @@ addEventListener('keydown', function(event) {
       isMoveBackward = false;
    }
    else if(event.code == 'Space'){
-      console.log("FIRE");
+      //console.log("FIRE");
       isFiring = true;
    }
-   console.log("key " + event.code + " pressed");
+   //console.log("key " + event.code + " pressed");
  });
 
  addEventListener('keyup', function(event) {
    if (event.code == 'KeyA') {
-     console.log("turn left");
+     //console.log("turn left");
      isTurnLeft = false;
    }
    else if(event.code == 'KeyS'){
-      console.log("going backward");
+      //console.log("going backward");
       isMoveBackward = false;
    }
    else if(event.code == 'KeyD'){
-      console.log("turn right");
+      //console.log("turn right");
       isTurnRight = false;
    }
    else if(event.code == 'KeyW'){
-      console.log("going forwad");
+      //console.log("going forwad");
       isMoveForward = false;
    }
    else if(event.code == 'Space'){
-      console.log("FIRE");
+      //console.log("FIRE");
       isFiring = false;
    }
-   console.log("key " + event.code + " pressed");
+   //console.log("key " + event.code + " pressed");
  });
 
 /** loading models */
@@ -238,7 +279,7 @@ bullet_pref = load_model("/assets/bullet.obj");
 async function instantiateTank(){
    tank = tank_pref.clone(true);
    tank.position.z = -1;
-   tank.position.y = 0.5;
+   tank.position.y = 0.2;
    console.log(tank);
    scene.add(tank);
    cameraLookatObj.position.copy(tank.position);
@@ -258,6 +299,33 @@ setTimeout(() => {
    canFire = true;
 }, 1000);
 
+
+// collision detection
+function obstacleDetection(){
+   var direction = new THREE.Vector3();
+   var targetPosition = new THREE.Vector3();
+   targetPosition.copy(tank.position);
+   tank.getWorldDirection(direction);
+
+   targetPosition.x += -(direction.x)*currentSpeed;
+   targetPosition.z += -(direction.z)*currentSpeed;
+   
+   //console.log(currentSpeed);
+
+   var raycastDirection = new THREE.Vector3();
+   raycastDirection.subVectors(targetPosition, tank.position);
+   raycaster.set(tank.position, raycastDirection);
+   raycaster.far = 0.3;
+   raycaster.near = 0.01;
+   var hits = raycaster.intersectObjects(obstacles.children, false);
+   // return nearest hit point or null if not hit
+   //console.log(hits);
+   if (hits.length > 0){
+      return hits[0].object.position;
+   }
+   else return null;
+}
+
 // animation
 
 var frameRate = 60;
@@ -276,7 +344,7 @@ function render(time){
          */
       }
       orbitControls.target.copy(tank.position);
-      console.log(orbitControls);
+      
       
       //camera.lookAt(tank.position);
 
@@ -301,22 +369,30 @@ function render(time){
 
    /** vertical speed */
    if (isMoveForward){
-      currentSpeed = THREE.MathUtils.lerp(currentSpeed, maxSpeed, 0.1);
+      currentSpeed = THREE.MathUtils.lerp(currentSpeed, maxSpeed, 0.02);
    }
    else if(isMoveBackward){
-      currentSpeed = THREE.MathUtils.lerp(currentSpeed, -maxSpeed, 0.1);
+      currentSpeed = THREE.MathUtils.lerp(currentSpeed, -maxSpeed, 0.02);
    }
    else {
-      currentSpeed = THREE.MathUtils.lerp(currentSpeed, 0, 0.1);
+      currentSpeed = THREE.MathUtils.lerp(currentSpeed, 0, 0.01);
    }
    
    var turningAngle = 0;
    /** rotation */
    if (isTurnLeft){
-      turningAngle = 1;
+      if(isMoveBackward){
+         turningAngle = -1;
+      } else{
+         turningAngle = 1;
+      }
    }
    else if(isTurnRight){
-      turningAngle = -1;
+      if(isMoveBackward){
+         turningAngle = 1;
+      } else{
+         turningAngle = -1;
+      }
    }
 
    /** FIRE */
@@ -329,9 +405,28 @@ function render(time){
       }, bullet_fire_interval);
    }
 
+   if(tank != null){
+      var hitPoint = obstacleDetection();
+      if (hitPoint == null){
+            /** update tank rotation and position */
+         if(turningAngle != 0){
+            tank.rotateY(THREE.MathUtils.DEG2RAD*turningAngle);
+         }
+         if(currentSpeed != 0){
+            var targetPosition = new THREE.Vector3();
+            targetPosition.copy(tank.position);
+            var direction = new THREE.Vector3();
+            tank.getWorldDirection(direction);
 
+            targetPosition.x += -(direction.x)*currentSpeed;
+            targetPosition.z += -(direction.z)*currentSpeed;
+            tank.position.copy(targetPosition);
+         }
+      }
+   }
 
    /** update tank rotation and position */
+   /*
    if(turningAngle != 0){
       tank.rotateY(THREE.MathUtils.DEG2RAD*turningAngle);
    }
@@ -345,6 +440,7 @@ function render(time){
       targetPosition.z += -(direction.z)*currentSpeed;
       tank.position.copy(targetPosition);
    }
+   */
 
    orbitControls.update();
 
